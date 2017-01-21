507d393b64b9b1dc8470b36457c56376838f5374
Meliton G. Lazaro <melitonlazaro1@gmail.com>
2017-01-21 10:07:16 +0800

    Add files via upload

---
+++ application/views/home.php
@@ -0,0 +1,43 @@
+
+
+<html>
+<head>
+        <title>Team 8</title>
+</head>
+<body>
+	<div align = "right"> 
+	<p>You are logged in as: <?php echo $this->session->userdata('username');?></p>
+	<?php $attribute = array('id' =>'logout_form',
+									 'class'=>'form-horizontal');?>
+			<?php echo form_open('Users/logout', $attribute);?>
+			<?php $data = array("value" => 'Logout',
+								"name" => 'submit');?>
+			<?php echo form_submit($data);?>
+			<?php echo form_close();?></div>
+	
+	<a href= "<?php echo site_url();?>/Users/add/">Add Thread </a>
+	<h3> List of Threads </h3>
+		<?php foreach($thread as $threads):
+			echo form_open('Users/login'); 
+			echo "<input type='hidden' name='thread_id' value='$threads->thread_id'>";
+			echo "</form>";
+			echo "Username: $threads->username<br>";
+			echo "Title: $threads->Title<br>";
+			
+			echo "Category: $threads->Category<br>";
+			echo "Date: $threads->Date<br>"; ?>
+			<?php if($this->session->userdata('username') === $threads->username):?>
+			<a href= "<?php echo site_url();?>/Users/get_thread/<?php echo $threads->thread_id; ?>"> Edit Thread </a>&nbsp&nbsp&nbsp
+			<a href= "<?php echo site_url();?>/Users/delete_thread/<?php echo $threads->thread_id; ?>"> Delete Thread </a>&nbsp&nbsp&nbsp
+			<?php endif;?>
+			<a href= "<?php echo site_url();?>/Users/view_thread/<?php echo $threads->thread_id; ?>">View Thread </a>
+		<?php	echo "<br><br>";
+			endforeach;
+		?>
+			
+			
+		
+		
+		
+</body>
+</html>
---
+++ application/views/view.php
@@ -0,0 +1,44 @@
+
+<div align = "right">
+	<p>You are logged in as: <?php echo $this->session->userdata('username');?></p>
+	<?php $attribute = array('id' =>'logout_form',
+									 'class'=>'form-horizontal');?>
+			<?php echo form_open('Users/logout', $attribute);?>
+			<?php $data = array("value" => 'Logout',
+								"name" => 'submit');?>
+			<?php echo form_submit($data);?>
+			<?php echo form_close();?></div>
+			<a href= "<?php echo site_url();?>/Users/start/?>">Back </a>
+	<br>
+	<h3>Thread Title</h3>
+			<?php echo validation_errors();?>
+			<?php echo form_open('Users/edit_thread/'); ?>
+			<input type = "hidden" name = "thread_id" value = "<?php echo $threads->thread_id;?>">
+		
+		<p><?php echo $threads->Title;?></p>
+		<p>Created by:<?php echo $threads->username;?></p>
+	<div class = "container">
+	<h3>Description </h3>
+	<p><?php echo $threads->Description;?></p>
+		</div>
+		<p>Category: <?php echo $threads->Category?></p>
+		<p>Date Created: <?php echo $threads->Date?></p>
+		
+		
+		<?php 
+		foreach ($posts as $post){
+			
+			echo "Username: $post->username<br>";
+			echo "Title: $post->date<br>";
+			
+			echo "Category: $post->Content<br>";
+		}
+		 ?>
+	
+		
+	
+	<br><br>
+	
+	<br><br>
+	</form>
+	
